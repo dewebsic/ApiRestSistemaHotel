4620d54d325be3758f0cfb4150d179ee202f942c
angel <dewebsic@gmail.com>
2021-05-07 19:31:04 -0400

    Initial commit

---
+++ src/main/java/com/angelsepulveda/apirestsistemahotel/ApiRestSistemaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.angelsepulveda.apirestsistemahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSistemaHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestSistemaHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angelsepulveda/apirestsistemahotel/ApiRestSistemaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelsepulveda.apirestsistemahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSistemaHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
